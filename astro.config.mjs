a5c0c1acf92365c0e7ff2397576fcdae0603c0f9
Yerson Garcia <Yersoncampo79@gmail.com>
2025-03-01 09:16:26 -0500

    navbar and taildwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+
+import netlify from "@astrojs/netlify";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+
+  output: "server",
+  adapter: netlify(),
+  integrations: [icon()],
 });
